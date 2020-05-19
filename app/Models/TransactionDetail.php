bb0076028544f85780fed009099299ddcdab37af
supardi9512 <supardi9512@gmail.com>
2020-05-20 01:57:19 +0700

    membuat model transaksi dan detail transaksi

---
+++ app/Models/TransactionDetail.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TransactionDetail extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'products_id', 'transaction_id'
+    ];
+
+    protected $hidden = [
+
+    ];
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class, 'transactions_id', 'id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'products_id', 'id');
+    }
+}
