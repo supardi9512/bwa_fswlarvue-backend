b11dfc0716ce8093845b98621912260ed7ed7778
supardi9512 <supardi9512@gmail.com>
2020-05-21 23:53:30 +0700

    membuat update transaksi

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -76,7 +76,11 @@ class TransactionController extends Controller
      */
     public function edit($id)
     {
-        //
+        $item = Transaction::findOrFail($id);
+
+        return view('pages.transactions.edit')->with([
+            'item' => $item
+        ]);
     }
 
     /**
@@ -88,7 +92,12 @@ class TransactionController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $data = $request->all();
+
+        $item = Transaction::findOrFail($id);
+        $item->update($data);
+        
+        return redirect()->route('transactions.index');
     }
 
     /**
