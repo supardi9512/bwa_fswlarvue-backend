3231e8db6ae59723272d65f1855da4af6e222f3f
supardi9512 <supardi9512@gmail.com>
2020-05-22 00:05:48 +0700

    membuat change status transaksi

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,5 @@ Route::resource('products', 'ProductController');
 
 Route::resource('product-galleries', 'ProductGalleryController');
 
+Route::get('transactions/{id}/set-status', 'TransactionController@setStatus')->name('transactions.status');
 Route::resource('transactions', 'TransactionController');
